f7c2f643ca5e112c0f9388d8b70e46c42b167249
Joshika Geddam <joshikagaddam@gmail.com>
2025-03-09 17:48:16 -0500

    Time: 31 ms (66.43%) | Memory: 58.4 MB (77.26%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -3,9 +3,9 @@ class Solution {
         if(nums.length==0||k<0){
             return new int[0];
         }
+        Deque<Integer> deque=new ArrayDeque<>();
         int n=nums.length;
         int[] result=new int[n-k+1];
-        Deque<Integer> deque=new ArrayDeque<>();
         for(int i=0;i<nums.length;i++){
             if(!deque.isEmpty() && deque.peekFirst()<i-k+1){
                 deque.pollFirst();
