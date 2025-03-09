0d5aec3e46e29a1fdf8b328d3df315d178fa959d
Joshika Geddam <joshikagaddam@gmail.com>
2025-03-09 16:53:51 -0500

    Time: 22 ms (31.98%) | Memory: 45.3 MB (49.40%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.length()==0||t.length()==0){
+            return "";
+        }
+        HashMap<Character,Integer> tcount=new HashMap<>();
+        HashMap<Character,Integer> windowCount=new HashMap<>();
+        for(char c:t.toCharArray()){
+            tcount.put(c,tcount.getOrDefault(c,0)+1);
+        }
+        int formed=0;
+        int required=tcount.size();
+        int l=0,r=0;
+        int minLen=Integer.MAX_VALUE;
+        int minLeft=0;
+        while(r<s.length()){
+            char c=s.charAt(r);
+            windowCount.put(c,windowCount.getOrDefault(c,0)+1);
+            if(tcount.containsKey(c) && windowCount.get(c).intValue()==tcount.get(c).intValue()){
+                formed++;
+            }
+            while(required==formed){
+                if(r-l+1<minLen){
+                    minLen=r-l+1;
+                    minLeft=l;
+                }
+                char leftMax=s.charAt(l);
+                windowCount.put(leftMax,windowCount.get(leftMax)-1);
+                if(tcount.containsKey(leftMax) && windowCount.get(leftMax)<tcount.get(leftMax)){
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return (minLen==Integer.MAX_VALUE)?"":s.substring(minLeft,minLeft+minLen);
+        
+    }
+}
