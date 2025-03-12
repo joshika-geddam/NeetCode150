d10cf3c95f120b9dfed4f61329eba05b403fa6cf
Joshika Geddam <joshikagaddam@gmail.com>
2025-03-12 11:26:51 -0500

    Time: 20 ms (45.59%) | Memory: 45.2 MB (49.47%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length()==0||t.length()==0){
+        if(s.length()==0 || t.length()==0){
             return "";
         }
         HashMap<Character,Integer> tcount=new HashMap<>();
@@ -8,11 +8,11 @@ class Solution {
         for(char c:t.toCharArray()){
             tcount.put(c,tcount.getOrDefault(c,0)+1);
         }
-        int formed=0;
         int required=tcount.size();
-        int l=0,r=0;
+        int formed=0;
         int minLen=Integer.MAX_VALUE;
         int minLeft=0;
+        int l=0,r=0;
         while(r<s.length()){
             char c=s.charAt(r);
             windowCount.put(c,windowCount.getOrDefault(c,0)+1);
@@ -24,15 +24,16 @@ class Solution {
                     minLen=r-l+1;
                     minLeft=l;
                 }
-                char leftMax=s.charAt(l);
-                windowCount.put(leftMax,windowCount.get(leftMax)-1);
-                if(tcount.containsKey(leftMax) && windowCount.get(leftMax)<tcount.get(leftMax)){
+                char ch=s.charAt(l);
+                windowCount.put(ch,windowCount.get(ch)-1);
+                if(tcount.containsKey(ch) && windowCount.get(ch)< tcount.get(ch)){
                     formed--;
                 }
                 l++;
             }
             r++;
         }
-        return (minLen==Integer.MAX_VALUE)?"":s.substring(minLeft,minLeft+minLen);
+        return (minLen==Integer.MAX_VALUE)?"":s.substring(minLeft,minLen+minLeft);
+        
     }
 }
