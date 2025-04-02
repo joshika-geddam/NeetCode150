3ce16b8085d7bd33ed0440e0a0685b8097f07c3e
Joshika Geddam <joshikagaddam@gmail.com>
2025-04-02 12:50:30 -0500

    Time: 19 ms (52.27%) | Memory: 45.7 MB (16.04%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length()==0 || t.length()==0){
+        if(t.length()==0 || s.length()==0){
             return "";
         }
         HashMap<Character,Integer> tcount=new HashMap<>();
         HashMap<Character,Integer> windowCount=new HashMap<>();
         for(char c:t.toCharArray()){
             tcount.put(c,tcount.getOrDefault(c,0)+1);
-        }
-        int required=tcount.size();
-        int formed=0;
+        } 
+        int left=0;
+        int right=0;
         int minLen=Integer.MAX_VALUE;
         int minLeft=0;
-        int l=0,r=0;
-        while(r<s.length()){
-            char c=s.charAt(r);
+        int required=tcount.size();
+        int formed=0;
+        while(right<s.length()){
+            char c=s.charAt(right);
             windowCount.put(c,windowCount.getOrDefault(c,0)+1);
-            if(tcount.containsKey(c) && windowCount.get(c).intValue()==tcount.get(c).intValue()){
+            if(tcount.containsKey(c)&& windowCount.get(c).intValue()==tcount.get(c).intValue()){
                 formed++;
             }
             while(required==formed){
-                if(r-l+1<minLen){
-                    minLen=r-l+1;
-                    minLeft=l;
+                if(right-left+1<minLen){
+                    minLen=right-left+1;
+                    minLeft=left;
                 }
-                char ch=s.charAt(l);
-                windowCount.put(ch,windowCount.get(ch)-1);
-                if(tcount.containsKey(ch) && windowCount.get(ch)< tcount.get(ch)){
+                char leftMax=s.charAt(left);
+                windowCount.put(leftMax,windowCount.get(leftMax)-1);
+                if(tcount.containsKey(leftMax)&& windowCount.get(leftMax)<tcount.get(leftMax)){
                     formed--;
                 }
-                l++;
+                left++;
             }
-            r++;
+            right++;
         }
-        return (minLen==Integer.MAX_VALUE)?"":s.substring(minLeft,minLen+minLeft);
+        return (minLen==Integer.MAX_VALUE)?"":s.substring(minLeft,minLeft+minLen);
         
     }
 }
