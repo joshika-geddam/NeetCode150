4e7259688748ad064d4c684676eb13b5e0a51862
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-21 14:24:26 -0500

    Time: 22 ms (30.54%) | Memory: 45.6 MB (27.75%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,23 +1,23 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length()==0 || s.length()==0){
+        if(t.length()==0||s.length()==0){
             return "";
         }
-        HashMap<Character,Integer> tcount=new HashMap<>();
         HashMap<Character,Integer> windowCount=new HashMap<>();
+        HashMap<Character,Integer> tcount=new HashMap<>();
         for(char c:t.toCharArray()){
             tcount.put(c,tcount.getOrDefault(c,0)+1);
-        } 
+        }
+        int required=tcount.size();
+        int formed=0;
         int left=0;
         int right=0;
         int minLen=Integer.MAX_VALUE;
         int minLeft=0;
-        int required=tcount.size();
-        int formed=0;
         while(right<s.length()){
             char c=s.charAt(right);
             windowCount.put(c,windowCount.getOrDefault(c,0)+1);
-            if(tcount.containsKey(c)&& windowCount.get(c).intValue()==tcount.get(c).intValue()){
+            if(tcount.containsKey(c)&&windowCount.get(c).intValue()==tcount.get(c).intValue()){
                 formed++;
             }
             while(required==formed){
@@ -27,7 +27,7 @@ class Solution {
                 }
                 char leftMax=s.charAt(left);
                 windowCount.put(leftMax,windowCount.get(leftMax)-1);
-                if(tcount.containsKey(leftMax)&& windowCount.get(leftMax)<tcount.get(leftMax)){
+                if(tcount.containsKey(leftMax) && windowCount.get(leftMax)<tcount.get(leftMax)){
                     formed--;
                 }
                 left++;
@@ -35,6 +35,5 @@ class Solution {
             right++;
         }
         return (minLen==Integer.MAX_VALUE)?"":s.substring(minLeft,minLeft+minLen);
-        
     }
 }
