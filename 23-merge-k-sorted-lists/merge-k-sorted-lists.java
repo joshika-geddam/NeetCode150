5114770f8a6e33a30e80d219cfcafb302a6d730f
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-21 16:55:53 -0500

    Time: 1 ms (99.89%) | Memory: 44.2 MB (95.93%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,40 +10,42 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null || lists.length==0){
+        if(lists==null||lists.length==0){
             return null;
         }
         return mergeKLists(lists,0,lists.length-1);
     }
-    public ListNode mergeKLists(ListNode[] lists,int start,int end){
-        if(start==end){
-            return lists[start];
+    public ListNode mergeKLists(ListNode[] lists,int left,int right){
+        if(left==right){
+            return lists[left];
         }
-        int mid=start+(end-start)/2;
-        ListNode left=mergeKLists(lists,start,mid);
-        ListNode right=mergeKLists(lists,mid+1,end);
-        return merge(left,right);
+        int mid=left+(right-left)/2;
+        ListNode l1=mergeKLists(lists,left,mid);
+        ListNode l2=mergeKLists(lists,mid+1,right);
+        return merge(l1,l2);
     }
     public ListNode merge(ListNode l1,ListNode l2){
-        ListNode dummy=new ListNode(0);
-        ListNode current=dummy;
+        ListNode dummy=new ListNode(-1);
+        ListNode ptr=dummy;
         while(l1!=null && l2!=null){
-            if(l1.val<=l2.val){
-                dummy.next=l1;
-                l1=l1.next;
-            }
-            else{
-                dummy.next=l2;
-                l2=l2.next;
-            }
-            dummy=dummy.next;
-        }
+        if(l1.val<=l2.val){
+            dummy.next=l1;
+            l1=l1.next;
+        }  
+        else{
+            dummy.next=l2;
+            l2=l2.next;
+        } 
+        dummy=dummy.next;
+        } 
+
         if(l1==null){
             dummy.next=l2;
         }
         if(l2==null){
             dummy.next=l1;
         }
-        return current.next;
+        return ptr.next;
+
     }
 }
