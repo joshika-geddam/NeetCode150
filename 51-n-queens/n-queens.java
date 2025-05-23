ebe76df5928b22b6083dffd52040516c40bbb627
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-23 16:40:33 -0500

    Time: 6 ms (21.50%) | Memory: 44.9 MB (56.93%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,23 +1,23 @@
 class Solution {
-    HashSet<Integer> col=new HashSet<>();
-    HashSet<Integer> pos=new HashSet<>();
-    HashSet<Integer> neg=new HashSet<>();
-    List<List<String>> res =new ArrayList<>();
+     List<List<String>> result=new ArrayList<>();
+     HashSet<Integer> col=new HashSet<>();
+     HashSet<Integer> pos=new HashSet<>();
+     HashSet<Integer> neg=new HashSet<>();
     public List<List<String>> solveNQueens(int n) {
         char[][] board=new char[n][n];
-        for(char[] r:board){
-            Arrays.fill(r,'.');
+        for(char[] row:board){
+            Arrays.fill(row,'.');
         }
-        backtrack(0,n,board);
-        return res;
+        backtrack(0,board,n);
+        return result;  
     }
-    public void backtrack(int r,int n,char[][] board){
+    public void backtrack(int r,char[][] board,int n){
         if(r==n){
             List<String> copy=new ArrayList<>();
             for(char[] row:board){
                 copy.add(new String(row));
             }
-            res.add(copy);
+            result.add(copy);
             return;
         }
         for(int c=0;c<n;c++){
@@ -27,8 +27,9 @@ class Solution {
             col.add(c);
             pos.add(r+c);
             neg.add(r-c);
-            board[r][c]='Q';
-            backtrack(r+1,n,board);
+                        board[r][c]='Q';
+
+            backtrack(r+1,board,n);
             col.remove(c);
             pos.remove(r+c);
             neg.remove(r-c);
