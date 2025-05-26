cd3a8b1c25c1048ab4ed8e1240453abe6664ff6a
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-26 18:00:19 -0500

    Time: 6 ms (12.65%) | Memory: 45.3 MB (11.96%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        List<List<Integer>> adj=new ArrayList<>();
         int n=edges.length;
+        List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int edge[]:edges){
+        for(int[] edge:edges){
             int u=edge[0];
             int v=edge[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
             boolean[] visit=new boolean[n+1];
-            if(dfs(u,-1,visit,adj)){
+            if(dfs(u,-1,adj,visit)){
                 return edge;
             }
         }
-        return new int[0];
+        return new int[]{};
     }
-    public boolean dfs(int node,int parent,boolean[] visit,List<List<Integer>> adj){
+    public boolean dfs(int node,int parent,List<List<Integer>> adj, boolean[] visit){
         if(visit[node]){
             return true;
         }
@@ -26,10 +26,11 @@ class Solution {
             if(neighbor==parent){
                 continue;
             }
-            if(dfs(neighbor,node,visit,adj)){
+            if(dfs(neighbor,node,adj,visit)){
                 return true;
             }
         }
         return false;
     }
+
 }
