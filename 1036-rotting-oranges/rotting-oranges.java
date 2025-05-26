83406200c632faf4299bc38d95864abd33a11c39
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-26 13:25:39 -0500

    Time: 2 ms (68.37%) | Memory: 42.8 MB (30.17%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -4,8 +4,9 @@ class Solution {
         int col=grid[0].length;
         Queue<int[]> queue=new LinkedList<>();
         int fresh=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
                     queue.add(new int[]{i,j});
                 }
@@ -20,8 +21,8 @@ class Solution {
         int minutes=0;
         int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
         while(!queue.isEmpty()){
-            int size=queue.size();
             boolean rot=false;
+            int size=queue.size();
             for(int i=0;i<size;i++){
                 int[] curr=queue.poll();
                 int r=curr[0];
@@ -29,13 +30,13 @@ class Solution {
                 for(int[] dir:directions){
                     int newR=r+dir[0];
                     int newC=c+dir[1];
-                    while(newR>=0 && newR<row && newC>=0 && newC<col &&
-                    grid[newR][newC]==1){
+                    while(newR>=0 && newC>=0 && newR<row && newC<col && grid[newR][newC]==1){
                         grid[newR][newC]=2;
                         queue.add(new int[]{newR,newC});
                         fresh--;
                         rot=true;
                     }
+
                 }
             }
             if(rot){
@@ -43,6 +44,5 @@ class Solution {
             }
         }
         return (fresh==0)?minutes:-1;
-        
     }
 }
