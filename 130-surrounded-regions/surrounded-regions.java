97812b2bbe636f8d58193f254a6ad283dadc56ca
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-26 13:44:01 -0500

    Time: 2 ms (84.23%) | Memory: 45.6 MB (38.56%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -4,18 +4,18 @@ class Solution {
         int col=board[0].length;
         for(int r=0;r<row;r++){
             if(board[r][0]=='O'){
-                dfs(board,r,0);
+                dfs(r,0,board);
             }
             if(board[r][col-1]=='O'){
-                dfs(board,r,col-1);
+                dfs(r,col-1,board);
             }
         }
         for(int c=0;c<col;c++){
             if(board[0][c]=='O'){
-                dfs(board,0,c);
+                dfs(0,c,board);
             }
             if(board[row-1][c]=='O'){
-                dfs(board,row-1,c);
+                dfs(row-1,c,board);
             }
         }
         for(int i=0;i<row;i++){
@@ -23,21 +23,21 @@ class Solution {
                 if(board[i][j]=='O'){
                     board[i][j]='X';
                 }
-                else if(board[i][j]=='T'){
+                if(board[i][j]=='T'){
                     board[i][j]='O';
                 }
             }
         }
-        
     }
-    public void dfs(char[][] board,int i,int j){
+    public void dfs(int i,int j,char[][] board){
         if(i<0||j<0||i>=board.length||j>=board[0].length||board[i][j]!='O'){
             return;
         }
         board[i][j]='T';
-        dfs(board,i+1,j);  
-        dfs(board,i-1,j);  
-        dfs(board,i,j+1);
-        dfs(board,i,j-1);     
-        }
+        dfs(i+1,j,board);
+        dfs(i-1,j,board);
+        dfs(i,j+1,board);
+        dfs(i,j-1,board);
+    }
+
 }
