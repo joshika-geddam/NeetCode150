b6f2c4257547de9b78f14cf0689166870a3ed238
Joshika Geddam <joshikagaddam@gmail.com>
2025-05-25 19:31:29 -0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (47.46%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,13 +3,13 @@ class Solution {
         if(nums.length<2){
             return nums[0];
         }
-        int[] dp=new int[nums.length];
+        int n=nums.length;
+        int[] dp=new int[n];
         dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;i++){
+        dp[1]= Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
             dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
-        }
-        return dp[nums.length-1];
-
+        }  
+        return dp[n-1];     
     }
 }
