30d9f8ecb34c2567ebcba15d95604deda099c04b
Joshika Geddam <joshikagaddam@gmail.com>
2025-06-22 17:51:36 -0500

    Time: 64 ms (72.78%) | Memory: 57.4 MB (72.36%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -3,28 +3,27 @@ class Solution {
         if(heights==null||heights.length==0){
             return 0;
         }
-        Stack<Integer> stack=new Stack<>();
+        int i=0;
         int left=0;
         int right=heights.length-1;
-        int i=0;
+        Stack<Integer> stack=new Stack<>();
         int maxArea=0;
         while(i<heights.length){
-            if(stack.isEmpty()|| heights[i]>=heights[stack.peek()]){
+            if(stack.isEmpty()||heights[i]>=heights[stack.peek()]){
                 stack.push(i++);
             }
             else{
                 int top=stack.pop();
-                int area=heights[top]*(stack.isEmpty()?i:i-stack.peek()-1);
-                maxArea=Math.max(maxArea,area);
+            int area=heights[top]*(stack.isEmpty()?i:i-stack.peek()-1);
+            maxArea=Math.max(area,maxArea);
             }
+            
         }
         while(!stack.isEmpty()){
-            
             int top=stack.pop();
-                int area=heights[top]*(stack.isEmpty()?i:i-stack.peek()-1);
-                maxArea=Math.max(maxArea,area);
+            int area=heights[top]*(stack.isEmpty()?i:i-stack.peek()-1);
+            maxArea=Math.max(area,maxArea);
         }
         return maxArea;
-        
     }
 }
