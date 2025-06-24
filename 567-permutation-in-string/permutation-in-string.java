3c1c620ab0cb697a2dceae7830b5eb005f28b4b5
Joshika Geddam <joshikagaddam@gmail.com>
2025-06-24 13:37:25 -0500

    Time: 5 ms (96.54%) | Memory: 42.7 MB (79.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int left=0,maxChar=s1.length();
+        int left=0;
         int right=0;
+        int maxOccur=s1.length();
         int[] freq=new int[26];
         for(char c:s1.toCharArray()){
             freq[c-'a']++;
         }
         while(right<s2.length()){
             if(freq[s2.charAt(right++)-'a']-->0){
-                maxChar--;
+                maxOccur--;
             }
-            if(maxChar==0){
+            if(maxOccur==0){
                 return true;
             }
-            else if(right-left==s1.length()&& freq[s2.charAt(left++)-'a']++>=0){
-                maxChar++;
+            else if(right-left==s1.length() && freq[s2.charAt(left++)-'a']++>=0){
+                maxOccur++;
             }
         }
         return false;
