0f2ed2a7cd2572d51e22a781b61b1b58c6ffe1b7
Joshika Geddam <joshikagaddam@gmail.com>
2025-06-28 12:37:28 -0500

    Time: 2 ms (69.90%) | Memory: 45.2 MB (19.05%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -3,21 +3,22 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int row=grid.length;
         int col=grid[0].length;
-        int maxArea=0;
         visited=new boolean[row][col];
+        int maxArea=0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1){
-                    maxArea=Math.max(maxArea,area(i,j,grid));
+                maxArea=Math.max(maxArea,area(i,j,grid));
                 }
             }
         }
-        return maxArea;
+        return maxArea;  
     }
     public int area(int i,int j,int[][] grid){
-        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]!=1||visited[i][j]){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]==0|| visited[i][j]){
             return 0;
         }
+        grid[i][j]=1;
         visited[i][j]=true;
         return 1+(area(i+1,j,grid)+area(i-1,j,grid)+area(i,j+1,grid)+area(i,j-1,grid));
     }
