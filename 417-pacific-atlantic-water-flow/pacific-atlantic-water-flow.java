53248aafcaf1945205da15954a54ea2ad9afad23
Joshika Geddam <joshikagaddam@gmail.com>
2025-06-28 14:11:45 -0500

    Time: 5 ms (59.56%) | Memory: 45.7 MB (46.72%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,17 +1,17 @@
 class Solution {
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    public List<List<Integer>> pacificAtlantic(int[][] height) {
         List<List<Integer>> result=new ArrayList<>();
-        int row=heights.length;
-        int col=heights[0].length;
+        int row=height.length;
+        int col=height[0].length;
         boolean[][] pacific=new boolean[row][col];
         boolean[][] atlantic=new boolean[row][col];
         for(int r=0;r<row;r++){
-            dfs(r,0,heights,pacific);
-            dfs(r,col-1,heights,atlantic);
+            dfs(r,0,height,pacific);
+            dfs(r,col-1,height,atlantic);
         }
         for(int c=0;c<col;c++){
-            dfs(0,c,heights,pacific);
-            dfs(row-1,c,heights,atlantic);
+            dfs(0,c,height,pacific);
+            dfs(row-1,c,height,atlantic);
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
@@ -22,15 +22,15 @@ class Solution {
         }
         return result;
     }
-    public void dfs(int row,int col,int[][] heights,boolean[][] visit){
+    public void dfs(int row,int col,int[][] height,boolean[][] visit){
         visit[row][col]=true;
-        int directions[][] ={{0,1},{1,0},{-1,0},{0,-1}};
+        int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
         for(int[] dir:directions){
             int newR=row+dir[0];
             int newC=col+dir[1];
-            while(newR>=0 && newR<heights.length && newC>=0 && newC<heights[0].length &&
-            heights[newR][newC]>=heights[row][col] && !visit[newR][newC]){
-                dfs(newR,newC,heights,visit);
+            while(newR>=0 && newR<height.length && newC>=0 && newC<height[0].length &&
+            height[newR][newC]>=height[row][col] && !visit[newR][newC]){
+                dfs(newR,newC,height,visit);
             }
         }
     }
