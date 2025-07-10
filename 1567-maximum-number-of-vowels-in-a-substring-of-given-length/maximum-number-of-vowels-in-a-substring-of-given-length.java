71c9e4745bd1cb2f2e5f64bfdea217a56e4977ce
Joshika Geddam <joshikagaddam@gmail.com>
2025-07-10 18:11:33 -0500

    Time: 11 ms (88.76%) | Memory: 45.7 MB (5.42%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        char[] v=s.toCharArray();
         int l=0;
-        int r=0;
         int count=0;
-        int maxCount=Integer.MIN_VALUE;
-        while(r<v.length){
-            if(v[r]=='a'||v[r]=='e'||v[r]=='i'||v[r]=='o'||v[r]=='u'){
+        int r=0;
+        char[] c=s.toCharArray();
+        int maxValue=Integer.MIN_VALUE;
+        while(r<s.length()){
+            if(c[r]=='a'||c[r]=='e'||c[r]=='i'||c[r]=='o'||c[r]=='u'){
                 count++;
             }
             if(r-l+1==k){
-                
-                maxCount=Math.max(maxCount,count);
-                  if(v[l]=='a'||v[l]=='e'||v[l]=='i'||v[l]=='o'||v[l]=='u'){
-                count--;
-            }              
+                maxValue=Math.max(maxValue,count);
+                if(c[l]=='a'||c[l]=='e'||c[l]=='i'||c[l]=='o'||c[l]=='u'){
+                    count--;
+                }
                 l++;
-
             }
             r++;
         }
-        return maxCount;
+        return maxValue;
     }
 }
