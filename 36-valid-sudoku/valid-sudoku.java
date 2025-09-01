0888db7822732d9ad8a359cb6a66d473bc3cb1a2
Joshika Geddam <joshikagaddam@gmail.com>
2025-09-01 17:12:21 -0500

    Time: 12 ms (19.83%) | Memory: 45 MB (20.69%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         HashSet<String> set=new HashSet<>();
         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
+            for(int j=0;j<board[i].length;j++){
                 char ch=board[i][j];
                 if(ch!='.'){
                     if(!set.add(ch+"is present in a row"+i)||
-                    !set.add(ch+"is present in a col"+j) ||
+                    !set.add(ch+"is present in a col"+j)||
                     !set.add(ch+"is present in a box"+i/3+'-'+j/3)){
                         return false;
                     }
@@ -14,5 +14,6 @@ class Solution {
             }
         }
         return true;
+        
     }
 }
