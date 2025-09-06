7cd91a069b18408144252c49e00c7c890e49f84b
Joshika Geddam <joshikagaddam@gmail.com>
2025-09-06 15:47:13 -0500

    Time: 21 ms (44.89%) | Memory: 45 MB (76.86%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -3,11 +3,11 @@ class Solution {
         int minSpeed=1;
         int maxSpeed=0;
         for(int pile:piles){
-            maxSpeed=Math.max(maxSpeed,pile);
+            maxSpeed=Math.max(pile,maxSpeed);
         }
         while(minSpeed<maxSpeed){
             int mid=minSpeed+(maxSpeed-minSpeed)/2;
-            if(canEat(piles,mid,h)){
+            if(canEat(piles,h,mid)){
                 maxSpeed=mid;
             }
             else{
@@ -16,7 +16,7 @@ class Solution {
         }
         return minSpeed;
     }
-    public boolean canEat(int[] piles,int mid,int h){
+    public boolean canEat(int[] piles,int h,int mid){
         int hours=0;
         for(int pile:piles){
             hours+=(int)Math.ceil((double)pile/mid);
