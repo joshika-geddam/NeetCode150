d640ddbb21b115ce87dd43a36812f13859f0341d
Joshika Geddam <joshikagaddam@gmail.com>
2025-10-06 19:02:43 -0500

    Time: 5 ms (77.68%) | Memory: 58.2 MB (23.86%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,8 +1,5 @@
 class Solution {
     public int maxArea(int[] height) {
-        if(height==null || height.length==0){
-            return 0;
-        }
         int maxArea=0;
         int left=0;
         int right=height.length-1;
@@ -12,7 +9,7 @@ class Solution {
             if(height[left]<height[right]){
                 left++;
             }
-            else{
+           else{
                 right--;
             }
         }
